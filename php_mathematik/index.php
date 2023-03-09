8cd85f47b13bcd1a7b0724bed0b38ca1d4c3bb84
Bahadir Biriktir <bahadirbrktr@gmail.com>
2023-03-09 22:52:30 +0100

    PHP

---
+++ php_mathematik/index.php
@@ -0,0 +1,57 @@
+<?php
+
+echo "multipliction:" . "<br>";
+echo (4 * 4)."<br>". "<br>"  ;
+
+echo "squere:" . "<br>" ;
+echo (5**2)."<br>". "<br>" ;
+
+echo "remainder of the percent:" . "<br>";
+echo (20 % 6)."<br>". "<br>" ;
+
+echo "addition and multipliction:" . "<br>";
+echo (3 + 3 * 7)."<br>". "<br>" ;
+
+echo "addition and multipliction with brackets:" . "<br>";
+echo ((3 + 3) * 7)."<br>". "<br>" ;
+
+echo "divide:" . "<br>";
+echo (20 / 4)."<br><br>";
+
+
+echo "write the process by defining a variable" . "<br>";
+echo "1010-1000=" . "<br>";
+$num = 1000;
+$num -= 10;
+echo $num."<br><br>";
+
+echo "990+1=" . "<br>";
+$num++;
+echo $num."<br><br>";
+
+echo "991-1=" . "<br>";
+$num--;
+echo $num."<br><br>";
+
+
+
+echo "the biggest number(5, 10):" . "<br>";
+echo max(5, 10)."<br><br>";
+
+echo "the smallest number(5, 10):" . "<br>";
+echo min(5, 10)."<br><br>";
+
+echo "the root of number(196):" . "<br>";
+echo sqrt(196)."<br><br>";
+
+echo "which number closer to which number (2.7):" . "<br>";
+echo round(2.7)."<br><br>";
+
+
+echo "rounded up number (3.4);" . "<br>";
+echo ceil(3.4)."<br><br>";
+
+echo "rounded down number (3.4);" . "<br>";
+echo floor(3.4)."<br>";
+
+?>
